dc7c5d18f75770c780e152d3e0fefb05a1c366d2
Simon Segundo <120813763+Simon-Segundo@users.noreply.github.com>
2025-11-19 22:59:39 -0500

    Drive Code V3.1.2
    Now I am working on the skills autonomous

--- 727R_PushBack/src/autons.cpp
+++ 727R_PushBack/src/autons.cpp
@@ -1,6 +1,8 @@
 #include "main.cpp" // IWYU pragma: keep
 #include "main.h" // IWYU pragma: keep
 #include "autons.hpp" // IWYU pragma: keep
+#include "autons.h"
+#include "globals.hpp"
 
 /* This file contains functions used during autonomous to control the robot
 in order to score points without user input. Code here should be optimized
@@ -42,4 +44,55 @@ void halfRightAuto() {
 // Autonomous function for skills challenge
 void skillsAuto() {
 
+    // Transfer
+    chassis.moveToPoint(0, 16, 250);
+    // Set of 3 Blocks
+    chassis.moveToPoint(-12, 53, 1500);
+    // Transfer
+    chassis.moveToPoint(-8, 37.5, 750, {.forwards = false});
+    unstore.move(-127);
+    pros::delay(500);
+    unstore.brake();
+    // Transfer
+    chassis.turnToHeading(-135, 500);
+    chassis.moveToPoint(-29, 20, 1250, {.maxSpeed = 80});
+    chassis.turnToHeading(180, 250);
+    // Match Load
+    matchLoad.set_value(true);
+    chassis.moveToPoint(-34.5, -5, 1000, {.maxSpeed = 50});
+    pros::delay(2500);
+    matchLoad.set_value(false);
+    // Transfer
+    chassis.moveToPoint(-32, 15, 1000, {.forwards = false});
+    chassis.turnToHeading(-20, 1250, {.maxSpeed = 80});
+    // High Goal
+    chassis.moveToPoint(-34, 34, 5000, {}, true);
+    pros::delay(500);
+    unstore.move(127);
+    storage.set_value(false);
+    oppStore = false;
+}
+
+void matchAutoRight () {
+    
+}
+
+void winPointAuto () {
+
+}
+
+void skillsAuto () {
+    storage.set_value(true);
+    oppStore = true;
+    intake.move(127);
+    // Transfer
+    chassis.moveToPoint(0, 16, 500);
+    chassis.turnToHeading(-90, 500);
+    chassis.moveToPoint(-34, 16, 1000);
+    matchLoad.set_value(true);
+    chassis.turnToHeading(180, 500);
+    // Match Load Left Red
+    chassis.moveToPoint(-34, 0, 1000);
+    pros::delay(500);
+    intake.brake();
 }
